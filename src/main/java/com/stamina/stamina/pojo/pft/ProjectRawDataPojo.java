7dba309e68161fac178f04b00c22a5491669a5f9
changxuanbin <374149940@qq.com>
2019-05-29 17:59:17 +0800

    实体

---
+++ src/main/java/com/stamina/stamina/pojo/pft/ProjectRawDataPojo.java
@@ -0,0 +1,126 @@
+package com.stamina.stamina.pojo.pft;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@DynamicUpdate
+@Table(name = "t_pft_projectrawdata")
+public class ProjectRawDataPojo {
+
+    /**
+     * 测试项目原始属性ID
+     */
+    @Id
+    @Column(name = "PROJECTRAWDATA_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long projectrawdataId;
+
+    /**
+     *项目名
+     */
+    @Column(name = "RAWPROJECT_NAME")
+    private String rawprojectName;
+
+    /**
+     *数值
+     */
+    @Column(name = "RAWPROJECT_VALUE")
+    private Long rawprojectValue;
+
+    /**
+     *单位
+     */
+    @Column(name = "RAWPROJECT_COMPANY")
+    private String rawprojectCompany;
+
+    /**
+     *测试时间
+     */
+    @Column(name = "RAWPROJECT_TIME")
+    private Date rawprojectTime;
+
+    /**
+     * 人员身份证code
+     */
+    @Column(name = "RAWPROJECT_PROPLECODE")
+    private String rawprojectProplecode;
+
+    /**
+     *批次code
+     */
+    @Column(name = "RAWPROJECT_BATCHCODE")
+    private String rawprojectBatchcode;
+
+    /**
+     *重复数据判定
+     */
+    @Column(name = "RAWPROJECT_REPEATDATA")
+    private String rawprojectRepeatdata;
+
+    public Long getProjectrawdataId() {
+        return projectrawdataId;
+    }
+
+    public void setProjectrawdataId(Long projectrawdataId) {
+        this.projectrawdataId = projectrawdataId;
+    }
+
+    public String getRawprojectName() {
+        return rawprojectName;
+    }
+
+    public void setRawprojectName(String rawprojectName) {
+        this.rawprojectName = rawprojectName == null ? null : rawprojectName.trim();
+    }
+
+    public Long getRawprojectValue() {
+        return rawprojectValue;
+    }
+
+    public void setRawprojectValue(Long rawprojectValue) {
+        this.rawprojectValue = rawprojectValue;
+    }
+
+    public String getRawprojectCompany() {
+        return rawprojectCompany;
+    }
+
+    public void setRawprojectCompany(String rawprojectCompany) {
+        this.rawprojectCompany = rawprojectCompany == null ? null : rawprojectCompany.trim();
+    }
+
+    public Date getRawprojectTime() {
+        return rawprojectTime;
+    }
+
+    public void setRawprojectTime(Date rawprojectTime) {
+        this.rawprojectTime = rawprojectTime;
+    }
+
+    public String getRawprojectProplecode() {
+        return rawprojectProplecode;
+    }
+
+    public void setRawprojectProplecode(String rawprojectProplecode) {
+        this.rawprojectProplecode = rawprojectProplecode == null ? null : rawprojectProplecode.trim();
+    }
+
+    public String getRawprojectBatchcode() {
+        return rawprojectBatchcode;
+    }
+
+    public void setRawprojectBatchcode(String rawprojectBatchcode) {
+        this.rawprojectBatchcode = rawprojectBatchcode == null ? null : rawprojectBatchcode.trim();
+    }
+
+    public String getRawprojectRepeatdata() {
+        return rawprojectRepeatdata;
+    }
+
+    public void setRawprojectRepeatdata(String rawprojectRepeatdata) {
+        this.rawprojectRepeatdata = rawprojectRepeatdata == null ? null : rawprojectRepeatdata.trim();
+    }
+}
---
+++ src/main/java/com/stamina/stamina/pojo/pft/TestersAttributePojo.java
@@ -0,0 +1,139 @@
+package com.stamina.stamina.pojo.pft;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@DynamicUpdate
+@Table(name = "t_pft_testersattribute")
+public class TestersAttributePojo {
+
+    /**
+     * 测试人员表ID
+     */
+    @Id
+    @Column(name = "TESTERSATTRIBUTE_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer testersattributeId;
+
+    /**
+     *姓名
+     */
+    @Column(name = "TESTERS_NAME")
+    private String testersName;
+
+    /**
+     *性别（0男、1女）
+     */
+    @Column(name = "TESTERS_GENDER")
+    private String testersGender;
+
+    /**
+     *身高
+     */
+    @Column(name = "TESTERS_HEIGHT")
+    private String testersHeight;
+
+    /**
+     *体重
+     */
+    @Column(name = "TESTERS_WEIGHT")
+    private String testersWeight;
+
+    /**
+     *身份证（验证位数）
+     */
+    @Column(name = "USER_IDCARD")
+    private String userIdcard;
+
+    /**
+     *RAWPROJECT_PEOPLECODE
+     */
+    @Column(name = "人员身份证code")
+    private String rawprojectPeoplecode;
+
+    /**
+     *总分
+     */
+    @Column(name = "TESTERS_TOTALSCORE")
+    private Long testersTotalscore;
+
+    /**
+     *批次code
+     */
+    @Column(name = "RAWPROJECT_BATCHCODE")
+    private String rawprojectBatchcode;
+
+    public Integer getTestersattributeId() {
+        return testersattributeId;
+    }
+
+    public void setTestersattributeId(Integer testersattributeId) {
+        this.testersattributeId = testersattributeId;
+    }
+
+    public String getTestersName() {
+        return testersName;
+    }
+
+    public void setTestersName(String testersName) {
+        this.testersName = testersName == null ? null : testersName.trim();
+    }
+
+    public String getTestersGender() {
+        return testersGender;
+    }
+
+    public void setTestersGender(String testersGender) {
+        this.testersGender = testersGender == null ? null : testersGender.trim();
+    }
+
+    public String getTestersHeight() {
+        return testersHeight;
+    }
+
+    public void setTestersHeight(String testersHeight) {
+        this.testersHeight = testersHeight == null ? null : testersHeight.trim();
+    }
+
+    public String getTestersWeight() {
+        return testersWeight;
+    }
+
+    public void setTestersWeight(String testersWeight) {
+        this.testersWeight = testersWeight == null ? null : testersWeight.trim();
+    }
+
+    public String getUserIdcard() {
+        return userIdcard;
+    }
+
+    public void setUserIdcard(String userIdcard) {
+        this.userIdcard = userIdcard == null ? null : userIdcard.trim();
+    }
+
+    public String getRawprojectPeoplecode() {
+        return rawprojectPeoplecode;
+    }
+
+    public void setRawprojectPeoplecode(String rawprojectPeoplecode) {
+        this.rawprojectPeoplecode = rawprojectPeoplecode == null ? null : rawprojectPeoplecode.trim();
+    }
+
+    public Long getTestersTotalscore() {
+        return testersTotalscore;
+    }
+
+    public void setTestersTotalscore(Long testersTotalscore) {
+        this.testersTotalscore = testersTotalscore;
+    }
+
+    public String getRawprojectBatchcode() {
+        return rawprojectBatchcode;
+    }
+
+    public void setRawprojectBatchcode(String rawprojectBatchcode) {
+        this.rawprojectBatchcode = rawprojectBatchcode == null ? null : rawprojectBatchcode.trim();
+    }
+}
